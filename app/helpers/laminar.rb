85c63d9804b896c470b5a1e1f32509bc03ad593d
Jordan Cole <stilist@ratafia.info>
2013-03-24 17:46:10 -0700

    start styling work

--- app/helpers/laminar.rb
+++ app/helpers/laminar.rb
@@ -1,29 +1,35 @@
 module Laminar
 	def item_classes data
-		"hentry hnews type-#{data["activity_type"]} source-#{data["source"]}"
+		hsl = calculate_hsl data
+		luminance = case
+			when hsl[:luminance] > 65 then "light"
+			when hsl[:luminance] < 45 then "dark"
+			else "mid"
+		end
+
+		"hentry hnews type-#{data["activity_type"]} source-#{data["source"]} #{luminance}"
 	end
 
 	def item_hsl data
+		hsl = calculate_hsl data
+		"background-color:hsl(#{hsl[:hue]}, #{hsl[:saturation]}%, #{hsl[:luminance]}%);"
+	end
+
+	private
+
+	def calculate_hsl data
 		date = data["created_at"]
 
 		# TODO Start from 240
 		hue = [360, date.yday].min
 
 		# TODO
-		conditions = {
-			sunny: 100,
-			partly_sunny: 80,
-			partly_cloudy: 70,
-			cloudy: 50,
-			rain: 30,
-			snow: 10
-		}
-		saturation = conditions[conditions.keys.sample]
+		saturation = data["data"]["id"][-2..-1].to_i
 
 		pct_of_day = (((date.hour * 60) + date.min) / 1400.0) * 100
 		# Peak at midday
 		luminance = (pct_of_day > 50 ? (100 - pct_of_day) : pct_of_day).floor
 
-		"background-color:hsl(#{hue}, #{saturation}%, #{luminance}%);"
+		{ hue: hue, saturation: saturation, luminance: luminance }
 	end
 end
