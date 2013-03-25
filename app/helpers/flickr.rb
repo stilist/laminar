39e2051ee57b2014bfc3aea0ebae4ad14e618bcd
Jordan Cole <stilist@ratafia.info>
2013-03-24 19:45:53 -0700

    namespace helpers; bump photo size back up

--- app/helpers/flickr.rb
+++ app/helpers/flickr.rb
@@ -1,15 +1,15 @@
 module Flickr
-	def person_url data
+	def self.person_url data
 		"http://www.flickr.com/photos/#{data["owner"]}"
 	end
 
-	def photo_url data
+	def self.photo_url data
 		# Detect stringified `Hash`
 		person = (data["owner"] =~ /{/) ? eval(data["owner"])["nsid"] : data["owner"]
 		"http://www.flickr.com/photos/#{person}/#{data["id"]}/"
 	end
 
-	def photo_source_url data, size="o"
+	def self.photo_source_url data, size="o"
 		data = eval(data) unless data.is_a? Hash
 		secret = (size == "o") ? data["originalsecret"] : data["secret"]
 		"http://farm#{data["farm"]}.staticflickr.com/#{data["server"]}/#{data["id"]}_#{secret}_#{size}.jpg"
