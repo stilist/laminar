619f8e8d4596e89e6648db14c74c1c9c110bdfd9
Jordan Cole <stilist@ratafia.info>
2013-03-25 09:59:03 -0700

    tweet media

--- app/helpers/twitter.rb
+++ app/helpers/twitter.rb
@@ -3,6 +3,19 @@ module Twitter
 		"https://twitter.com/#{data["user"]["screen_name"]}"
 	end
 
+	def self.media data
+		out = ""
+
+		media = eval(data["entities"])["media"]
+		media.each do |item|
+			out << "<a href='#{item["expanded_url"]}'>
+				<img src='#{item["media_url_https"]}'>
+			</a>"
+		end
+
+		out
+	end
+
 	def self.tweet_url data
 		person_url(data) << "/status/#{data["id_str"]}"
 	end
@@ -12,6 +25,9 @@ module Twitter
 
 		urls = eval(data["entities"])["urls"]
 		urls.each { |url| tweet[url["url"]] = url["expanded_url"] }
+
+		media = eval(data["entities"])["media"]
+		media.each { |url| tweet[url["url"]] = url["expanded_url"] }
 
 		out = tweet.gsub /@(\w+)/, %Q{@<a href="http://twitter.com/\\1">\\1</a>}
 
