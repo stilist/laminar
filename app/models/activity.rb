726cc1db2b0d295f29feba024a8d450e6a33c3f0
Jordan Cole <stilist@ratafia.info>
2013-04-02 00:24:43 -0700

    Activity#title/Activity#description (#23)

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Activity < ActiveRecord::Base
 	default_scope where(is_private: false).
 			order("activities.updated_at DESC, activities.id DESC")
@@ -21,5 +23,78 @@ class Activity < ActiveRecord::Base
 		# end
 
 		data
+	end
+
+	def title ; @title ||= open_graph[:title] end
+	def description ; @description ||= open_graph[:description] end
+
+	private
+
+	def h text
+		Rack::Utils.escape_html text
+	end
+
+	# This is kind of a monster
+	def open_graph
+		return { title: @title, description: @description } if @title && @description
+
+		title = description = ""
+		data = self.data
+
+		case self.source
+		when "flickr"
+			media_type = (data.has_key? "video") ? "video" : "photo"
+			if self.activity_type == "favorite"
+				title = "favorited a #{media_type}"
+
+				user = data["owner"]
+				username = user["realname"].empty? ? user["username"] : user["realname"]
+				description = "favorited #{h username}’s #{media_type} ‘#{h data["title"]["_content"]}’"
+			else
+				title = "posted a #{media_type}"
+
+				description = "posted the #{media_type} ‘#{h data["title"]}’"
+				description << ": ‘#{data["description"]}’" unless data["description"].empty?
+			end
+		when "netflix"
+			# TODO TV shows
+			title = "reviewed a movie"
+
+			description = "reviewed #{eval(data["title"])[:attributes][:regular]}"
+			description << "(#{data["release_year"]}): #{data["user_rating"].to_i} stars"
+		when "pinboard"
+			title = "bookmarked a URL"
+
+			description = "bookmarked"
+			description << data["description"].empty? ? locals["url"] : data["description"]
+		when "twitter"
+			if self.activity_type == "favorite"
+				user = data["user"].is_a?(String) ? eval(data["user"]) : data["user"]
+				title = "favorited #{user}’s tweet"
+			else
+				if data["retweeted_status"]
+					retweet = eval(data["retweeted_status"])
+					title = "retweeted #{retweet["user"]["name"]}"
+				else
+					title = "posted a tweet"
+					description = "tweeted #{Twttr::text data, false}"
+				end
+			end
+		# fallback
+		else
+			vowels = %w(a e i o u)
+			if vowels.include? self.activity_type[-1]
+				verb = "#{self.activity_type}d"
+			else
+				verb = "#{self.activity_type}ed"
+			end
+
+			# Intentionally leave `description` blank
+			title = "#{verb} on #{self.source}"
+		end
+
+		title = "#{title[0..69]}…" if title.length > 70
+		description = "#{description[0..199]}…" if description.length > 200
+		{ title: title, description: description }
 	end
 end
