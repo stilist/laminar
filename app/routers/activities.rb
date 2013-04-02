67efe5174214bc0f4528c8f6ade073dd9eb5d7b0
Jordan Cole <stilist@ratafia.info>
2013-04-02 00:25:03 -0700

    adjust activity show page to use title & description

--- app/routers/activities.rb
+++ app/routers/activities.rb
@@ -1,11 +1,19 @@
 App.configure do |app|
 	app.get "/activities/:id" do
-		@page_summary = @page_title
+		item = Activity.find_by_id params[:id]
+
+		@page_title = item ? item.title : 404
+		@page_summary = item ? item.description : ""
 		@page_type = :permalink
 		@permalink = "/activities/#{params[:id]}"
 
-		item = Activity.find_by_id params[:id]
-		@page_title = item ? "#{item["source"]} #{item["activity_type"]}" : 404
-		page_out item, ->{ 404 unless item }
+		if item
+			observations = Weather.prefetch item.created_at, item.created_at
+			extras = { "observations" => observations }
+		else
+			extras = {}
+		end
+
+		page_out item, ->{ 404 unless item }, extras
 	end
 end
