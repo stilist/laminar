ede207791a4c2477eecec42dbc0dd117b4bed130
Jordan Cole <stilist@ratafia.info>
2013-06-15 14:09:56 -0700

    #29 fix timeline for single activity

--- app/routers/activities.rb
+++ app/routers/activities.rb
@@ -1,20 +1,20 @@
 App.configure do |app|
 	app.get "/activities/:id" do
 		activities = Laminar.activities params
-		item = activities.find_by_id params[:id]
+		@item = activities.find_by_id params[:id]
 
-		@page_title = item ? item.title : 404
-		@page_summary = item ? item.description : ""
+		@page_title = @item ? @item.title : 404
+		@page_summary = @item ? @item.description : ""
 		@page_type = :permalink
 		@permalink = "/activities/#{params[:id]}"
 
 		extras = { "full_view" => true }
-		if item
-			observations = Weather.prefetch item.created_at, item.created_at
+		if @item
+			observations = Weather.prefetch @item.created_at, @item.created_at
 			extras.merge!({ "observations" => observations })
 		end
 
 		extras["private_data_key"] = params[:private_data_key]
-		page_out item, ->{ 404 unless item }, extras
+		page_out @item, ->{ 404 unless @item }, extras
 	end
 end
