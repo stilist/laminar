8d267d17e2a780b7bf36673ad03a3fe6f8fa4946
Jordan Cole <stilist@ratafia.info>
2013-06-23 07:49:48 -0700

    nothing uses LTumblr.client

--- app/helpers/l_tumblr.rb
+++ app/helpers/l_tumblr.rb
@@ -1,13 +1,4 @@
 module LTumblr
-	def self.client
-		Tumblr.configure do |config|
-			config.consumer_key = ENV["TUMBLR_API_KEY"]
-			config.consumer_secret = ENV["TUMBLR_API_SECRET"]
-			config.oauth_token = ENV["TUMBLR_CLIENT_KEY"]
-			config.oauth_token_secret = "access_token_secret"
-		end
-	end
-
 	def self.oauth_client
 		@client ||= OAuth::Consumer.new(ENV["TUMBLR_API_KEY"], ENV["TUMBLR_API_SECRET"], {
 			site: "http://www.tumblr.com",
